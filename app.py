d884fbd21bd03206f6c698f13dee9e523e3903c7
Mayyan124 <165564558+Mayyan124@users.noreply.github.com>
2026-03-05 12:15:19 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import os
+
+# הגדרת האפליקציה - אומרים לה איפה נמצאים דפי ה-HTML והעיצוב
+app = Flask(__name__, 
+            template_folder='frontend', 
+            static_folder='frontend/static')
+
+@app.route('/')
+def home():
+    # שולח את המשתמש לדף הבית
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/attacks')
+def attacks():
+    return render_template('attacks.html')
+
+@app.route('/mysteries')
+def mysteries():
+    return render_template('mysteries.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    # הרצה במצב בדיקה
+    app.run(debug=True)
